0e10bca3f6f44012a4ed677c82f9f29a7973f922
Pyetro <pyetroribeirosantacruz@gmail.com>
2025-08-20 09:03:35 -0300

    Created get lists

--- src/main/java/com/devprime/dslist/services/GameService.java
+++ src/main/java/com/devprime/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devprime.dslist.dto.GameDTO;
 import com.devprime.dslist.dto.GameMinDTO;
 import com.devprime.dslist.entities.Game;
+import com.devprime.dslist.projections.GameMinProjection;
 import com.devprime.dslist.repositories.GameRepository;
 
 @Service
@@ -27,6 +28,13 @@ public class GameService {
 	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
+		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+		return dto;
+	}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId){
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
 		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
 		return dto;
 	}
